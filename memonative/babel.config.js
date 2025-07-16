56afd926224c11a461419798872b52ba82c673b6
Divyam Anand <wrkdivan@gmail.com>
2025-07-16 16:06:28 +0530

    babel

--- memonative/babel.config.js
+++ memonative/babel.config.js
@@ -1,6 +1,14 @@
-api.cache(false)
-module.exports = {
-  plugins: [
-    ['module:react-native-dotenv']
-  ]
-};
+module.exports = function (api) {
+  api.cache(false)
+
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module:react-native-dotenv', {
+        moduleName: "@env",
+        path: ".env",
+        allowUndefined: true
+      }]
+    ]
+  }
+}
