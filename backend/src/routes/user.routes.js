0b115877fe75d3f2329f9abe4d8adc246db9c7d9
Divyam Anand <wrkdivan@gmail.com>
2025-07-22 23:51:46 +0530

    removed existing user check

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { currentUser, loginUser, logoutUser, refreshAccessToken, registerUser } from "../controller/user.controller.js";
-import { existingUserCheck } from "../middleware/existingCheck.middleware.js";
 import {verifyJWT} from "../middleware/verifyJWT.middleware.js"
 
 
 const router = Router()
 
-router.post("/register", existingUserCheck, registerUser)
-router.post("/login", existingUserCheck, loginUser)
+router.post("/register", registerUser)
+router.post("/login",  loginUser)
 router.post("/logout", verifyJWT, logoutUser)
 router.get("/getCurrentUser", verifyJWT, currentUser)
 router.post("/refresh-token", refreshAccessToken)
